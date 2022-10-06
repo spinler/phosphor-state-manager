d19fa4669385ce2c97ccb3f9a2a5cc4e6477c416
Andrew Geissler <geissonator@users.noreply.github.com>
2022-10-06 11:24:56 -0500

    1040 stop fsi scan (#23)
    * Stop FSI scan service if chassis power on but host not booting
    
    If the host is then started, CFAM reset will start so FSI needs to be
    scanned again, otherwise FSI access is broken.
    
    Signed-off-by: Eddie James <eajames@linux.ibm.com>
    Signed-off-by: Eddie James <eajames@us.ibm.com>

--- host_reset_recovery.cpp
+++ host_reset_recovery.cpp
@@ -32,6 +32,7 @@ constexpr auto HOST_STATE_SVC = "xyz.openbmc_project.State.Host";
 constexpr auto HOST_STATE_PATH = "/xyz/openbmc_project/state/host0";
 
 constexpr auto HOST_STATE_QUIESCE_TGT = "obmc-host-quiesce@0.target";
+constexpr auto FSI_SCAN_SVC = "fsi-scan@0.service";
 
 bool wasHostBooting(sdbusplus::bus_t& bus)
 {
@@ -138,6 +139,27 @@ void moveToHostQuiesce(sdbusplus::bus_t& bus)
     }
 }
 
+void stopFsiScan(sdbusplus::bus::bus& bus)
+{
+    try
+    {
+        auto method = bus.new_method_call(SYSTEMD_SERVICE, SYSTEMD_OBJ_PATH,
+                                          SYSTEMD_INTERFACE, "StopUnit");
+
+        method.append(FSI_SCAN_SVC, "replace");
+
+        bus.call_noreply(method);
+    }
+    catch (const sdbusplus::exception::exception& e)
+    {
+        error("sdbusplus call exception stopping FSI scan target: {ERROR}",
+              "ERROR", e);
+
+        throw std::runtime_error(
+            "Error in invoking D-Bus systemd StopUnit method");
+    }
+}
+
 } // namespace phosphor::state::manager
 
 int main()
@@ -177,6 +199,7 @@ int main()
     // the BMC reboot occurred
     if (!wasHostBooting(bus))
     {
+        stopFsiScan(bus);
         return 0;
     }
 
