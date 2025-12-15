486b0d23d264fd884e62b78e7acc1292ecc57cd9
Matt Spinler <spinler@us.ibm.com>
2025-12-15 14:46:27 -0600

    RBMC: Cache the FW version hash value
    There's no need to run the hashing functions more than once.
    
    Change-Id: Id7c587d1ba3dfac918541055259d57ad3ba8404d
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- redundant-bmc/src/services_impl.cpp
+++ redundant-bmc/src/services_impl.cpp
@@ -528,6 +528,13 @@ bool ServicesImpl::getProvisioned() const
 
 std::string ServicesImpl::getFWVersion() const
 {
+    static std::string hexVersionString;
+
+    if (!hexVersionString.empty())
+    {
+        return hexVersionString;
+    }
+
     std::ifstream versionFile{"/etc/os-release"};
     std::string line;
     std::string keyPattern{"VERSION_ID="};
@@ -565,8 +572,9 @@ std::string ServicesImpl::getFWVersion() const
     EVP_DigestUpdate(context.get(), version.c_str(), strlen(version.c_str()));
     EVP_DigestFinal(context.get(), digest.data(), nullptr);
 
-    return std::format("{:02X}{:02X}{:02X}{:02X}", digest[0], digest[1],
-                       digest[2], digest[3]);
+    hexVersionString = std::format("{:02X}{:02X}{:02X}{:02X}", digest[0],
+                                   digest[1], digest[2], digest[3]);
+    return hexVersionString;
 }
 
 SystemState ServicesImpl::getSystemState() const
