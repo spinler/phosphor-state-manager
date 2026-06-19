1de8b87dcf85121b674c805c28476a8490e235d3
Matt Spinler <spinler@us.ibm.com>
2026-06-19 15:25:57 -0500

    RBMC: Add Manager testcases
    Add testcases for the Manager class where the BMC becomes passive on
    startup.
    
    The mock classes all have a new setupDefaultBehaviors function to handle
    returning default values for their coroutine functions so the testcases
    themselves don't have to specify them unless they want a different one.
    
    Change-Id: I904b66138dc12c89f9afda9925192d162bd9eff3
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- redundant-bmc/test/mocks/mock_sibling_reset.hpp
+++ redundant-bmc/test/mocks/mock_sibling_reset.hpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
+#include "async_helpers.hpp"
 #include "sibling_reset.hpp"
 
 #include <gmock/gmock.h>
@@ -22,6 +23,17 @@ class MockSiblingReset : public testing::NiceMock<SiblingReset>
     MOCK_METHOD(void, assertReset, (), (override));
     MOCK_METHOD(void, releaseReset, (), (override));
     MOCK_METHOD(sdbusplus::async::task<>, toggleReset, (), (override));
+
+    /**
+     * @brief Setup default behaviors for common test scenarios to save
+     *        setup in the testcases.
+     */
+    void setupDefaultBehavior()
+    {
+        ON_CALL(*this, toggleReset()).WillByDefault([]() {
+            return test_helpers::makeCompletedTask();
+        });
+    }
 };
 
 } // namespace rbmc
--- redundant-bmc/test/mocks/mock_sync_interface.hpp
+++ redundant-bmc/test/mocks/mock_sync_interface.hpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
+#include "async_helpers.hpp"
 #include "sync_interface.hpp"
 
 #include <gmock/gmock.h>
@@ -22,6 +23,24 @@ class MockSyncInterface : public testing::NiceMock<SyncInterface>
     MOCK_METHOD(sdbusplus::async::task<bool>, doFullSync, (), (override));
     MOCK_METHOD(sdbusplus::async::task<>, disableBackgroundSync, (),
                 (override));
+
+    /**
+     * @brief Setup default behaviors for common test scenarios to save
+     *        setup in the testcases.
+     */
+    void setupDefaultBehavior()
+    {
+        ON_CALL(*this, doFullSync()).WillByDefault([this]() {
+            fullSyncComplete = true;
+            return test_helpers::makeCompletedTask(true);
+        });
+
+        ON_CALL(*this, disableBackgroundSync()).WillByDefault([]() {
+            return test_helpers::makeCompletedTask();
+        });
+    }
+
+    bool fullSyncComplete{false};
 };
 
 } // namespace rbmc
